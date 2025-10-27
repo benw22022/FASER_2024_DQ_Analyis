9ac120b7270226d38ccf2133bb600cc20f938730
bewilson <benjamin.james.wilson@cern.ch>
2025-10-27 14:18:20 +0000

    reorganise files into folders and remove unused files

--- submission_files/run_all.sh
+++ submission_files/run_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# mkdir -p output_2022/logs
+mkdir -p output_2022/logs
 mkdir -p output_2023/logs
 mkdir -p output_2024/logs
 
-# condor_submit DQ_2022.submit
+condor_submit DQ_2022.submit
 condor_submit DQ_2023.submit
 condor_submit DQ_2024.submit
