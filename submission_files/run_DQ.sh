ad89d9c45455963bf3c8d3c12c371064678bb9b1
bewilson <benjamin.james.wilson@cern.ch>
2025-10-30 14:04:02 +0000

    bug fix

--- submission_files/run_DQ.sh
+++ submission_files/run_DQ.sh
@@ -5,7 +5,7 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_107/x86_64-el9-gcc11-opt/setup.sh
 run=$1
 output_dir=$2
 
-source_dir=/home/ppd/bewilson/FASER_2024_DQ_Analyis/
+source_dir=/home/ppd/bewilson/work/FASER_2024_DQ_Analyis/
 
 cp ${source_dir}/RDFDefines.h .
-python3 ${source_dir}/FASER_DQ_RDF.py $run -o $output_dir -f ${source_dir}/faser_filelists -c ${source_dir}/histograms
+python3 ${source_dir}/FASER_DQ_RDF.py $run --output_file_dir $output_dir --input_file_list_dir ${source_dir}/faser_filelists --histograms ${source_dir}/histograms
